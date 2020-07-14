0641887ff5738f5b1255e5ac2deb80d3b5f52cf8
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-07-14 17:14:23 +0100

    added user identity and token

---
+++ twilio-chat/app/controllers/tokens_controller.rb
@@ -0,0 +1,25 @@
+class TokensController < ApplicationController
+  before_action :require_login
+  # a controller that should only be used by a logged in user
+
+  def create
+   # Define User Identity
+   identity = current_user.email
+
+   # Create Grant for Access Token
+   grant = Twilio::JWT::AccessToken::ChatGrant.new
+   grant.service_sid = ENV['TWILIO_CHAT_SERVICE_SID']
+
+   # Create an Access Token
+   token = Twilio::JWT::AccessToken.new(
+     ENV['TWILIO_ACCOUNT_SID'],
+     ENV['TWILIO_API_KEY'],
+     ENV['TWILIO_API_SECRET'],
+     [grant],
+     identity: identity
+   )
+
+   # Generate the token and send to client
+   render json: { identity: identity, token: token.to_jwt }
+  end
+end
--- twilio-chat/app/javascript/packs/application.js
+++ twilio-chat/app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+const Rails = require("@rails/ujs")
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -15,3 +17,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+Rails.ajax({
+  url: "/tokens",
+  type: "POST",
+    success: function(data) {
+      console.log(data);
+    } 
+});
